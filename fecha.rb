c1cada27bd761592189cb52ceb7e2c68896b73e5
Samuel Serna Wills <samuel.sernaw@gmail.com>
2015-01-21 20:06:08 -0500

    Se agrega test para validar las fechas

--- fecha.rb
+++ fecha.rb
@@ -14,4 +14,18 @@ dia=a[0].to_i
 mes=a[1].to_i
 year=a[2].to_i
 
+# Test para validar la fecha
+if mes <= 0 or mes > 12
+	puts "#{inv}"
+elsif mes == 2 
+	if dia > 29
+		puts "#{inv}"
+	end
+
+elsif mes == 4 or mes == 6 or mes == 9 or mes == 11
+	if dia > 30
+		puts "#{inv}"		
+	end
+end
+
 puts "Hoy es el #{dia} de #{meses[mes]} de #{year}"		
